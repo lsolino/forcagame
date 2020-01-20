30603ca53644aabf84c396f5c8571d7f86ce2f3c
Luan Solino <luansolino@gmail.com>
2020-01-20 17:15:54 -0300

    Refatorando a classe Palavra

--- src/forcagame/Palavra.java
+++ src/forcagame/Palavra.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Palavra extends ObjetoDominioImpl {
 
-    private List<Letra> palavra;
+    private List<Letra> palavras;
     private Tema tema;
     private static LetraFactory letraFactory;
     
@@ -18,38 +18,40 @@ public class Palavra extends ObjetoDominioImpl {
         if (factory == null) {
             throw new RuntimeException("A fábrica de Letra ainda não foi inicializada.");
         }
-
+        
         for (i = 0; i < palavra.length(); i++) {
-            this.palavra.add(factory.getLetra(palavra.charAt(i)));
+            this.palavras.add(factory.getLetra(palavra.charAt(i)));
         }
+        
         this.tema = tema;
+      
     }
     
     public List<Letra> getLetras() {
-        return Collections.unmodifiableList(this.palavra);
+        return Collections.unmodifiableList(this.palavras);
     }
     
     public Letra getLetra(int posicao) {
-        return this.palavra.get(posicao);
+        return this.palavras.get(posicao);
     }
     
     public void exibir(Object contexto) {
-        for (Letra letra: palavra) {
+        for (Letra letra: palavras) {
             letra.exibir(null);
         }
     }
     
     public void exibir(Object contexto, List<Boolean> posicoes) {
-        if (posicoes.size() != palavra.size()) {
+        if (posicoes.size() != palavras.size()) {
             throw new RuntimeException("Posicoes não tem o mesmo tamanho da palavra!");
         }
 
         LetraFactory factory = getLetraFactory();
         Letra letraEncoberta = factory.getLetraEncoberta();
 
-        for (int i = 0; i < palavra.size(); i++) {
+        for (int i = 0; i < palavras.size(); i++) {
             if (posicoes.get(i)) {
-                palavra.get(i).exibir(null);
+                palavras.get(i).exibir(null);
             } else {
                 letraEncoberta.exibir(null);
             }
@@ -59,8 +61,8 @@ public class Palavra extends ObjetoDominioImpl {
     public List<Integer> tentar(char codigo) {
         List<Integer> posicoesEncontradas = new ArrayList<>();
 
-        for (int i = 0; i < palavra.size(); i++) {
-            if (palavra.get(i).getCodigo() == codigo)
+        for (int i = 0; i < palavras.size(); i++) {
+            if (palavras.get(i).getCodigo() == codigo)
                 posicoesEncontradas.add(i);
         }
         return posicoesEncontradas;
@@ -72,8 +74,8 @@ public class Palavra extends ObjetoDominioImpl {
     
     public boolean comparar(String palavra) {
         int i;
-        for (i = 0; i < this.palavra.size(); i++) {
-            if (this.palavra.get(i).getCodigo() != palavra.charAt(i)) {
+        for (i = 0; i < this.palavras.size(); i++) {
+            if (this.palavras.get(i).getCodigo() != palavra.charAt(i)) {
                 return false;
             }
         }
@@ -81,7 +83,7 @@ public class Palavra extends ObjetoDominioImpl {
     }
     
     public int getTamanho(){
-        return this.palavra.size();
+        return this.palavras.size();
     }
     
     public static void setLetraFactory(LetraFactory factory){
