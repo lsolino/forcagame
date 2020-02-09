ab11d630302150d01a149f622878457989a499e1
Luan Solino <luansolino@gmail.com>
2020-02-09 18:44:09 -0300

    implementacao palavra service

--- src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/PalavraAppService.java
@@ -1,36 +1,50 @@
 package br.edu.iff.bancodepalavras.dominio.palavra;
 
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaFactory;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaFactoryImpl;
+import br.edu.iff.jogoforca.Aplicacao;
 import br.edu.iff.repository.RepositoryException;
 
 public class PalavraAppService {
 	
-	private static PalavraAppService soleInstance;
-	
-	private PalavraRepository repository;
-	private PalavraFactory factory;
-	
-	private PalavraAppService(PalavraRepository repository, PalavraFactory factory) {
-		this.repository = repository;
-		this.factory = factory;
-	}
-	
-	public static void createSoleInstance(PalavraRepository palavraRepository, PalavraFactory palavraFactory) {
-		soleInstance = new PalavraAppService(palavraRepository, palavraFactory);
-	}
-	
-	public static PalavraAppService getSoleInstance() {
-		if(soleInstance == null) {
-      soleInstance = new PalavraAppService(null, null);
+    private static PalavraAppService soleInstance;
+
+    private PalavraRepository repository;
+    private PalavraFactory factory;
+
+    private PalavraAppService(PalavraRepository repository, PalavraFactory factory) {
+        this.repository = repository;
+        this.factory = factory;
     }
-		
-		return soleInstance;
-	}
-	
-	public void novaPalavra(Palavra palavra) throws RepositoryException{
-		try {
-			repository.inserir(palavra);	
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
-		}
-	}
+
+    public static void createSoleInstance(PalavraRepository palavraRepository, PalavraFactory palavraFactory) {
+        soleInstance = new PalavraAppService(palavraRepository, palavraFactory);
+    }
+
+    public static PalavraAppService getSoleInstance() {
+        if(soleInstance == null) {
+            soleInstance = new PalavraAppService(null, null);
+          }
+
+        return soleInstance;
+    }
+
+    public boolean novaPalavra(String palavra) throws RepositoryException{
+        Palavra auxPalavra = repository.getPalavra(palavra);
+        Aplicacao aplicacao = Aplicacao.getSoleInstance();
+        if (auxPalavra == null) {
+            try {
+                TemaFactory temaFactory = aplicacao.getTemaFactory();
+                Tema tema = temaFactory.getTema("Geral");
+                Palavra novaPalavra = factory.getPalavra(palavra, tema);
+                repository.inserir(novaPalavra);
+                return true;
+            } catch (Exception e) {
+                System.out.print(e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
+++ src/br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
@@ -7,70 +7,70 @@ import br.edu.iff.repository.RepositoryException;
 
 public class RodadaAppService {
   
-	private static RodadaAppService soleInstance;
-	
-	private RodadaRepository rodadaRepository;
-	private RodadaFactory rodadaFactory;
-	private JogadorRepository jogadorRepository;
-	
-	private RodadaAppService(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
-		this.rodadaRepository = rodadaRepository;
-		this.rodadaFactory = rodadaFactory;
-		this.jogadorRepository = jogadorRepository;
-	}
-	
-	public void createSoleInstance(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
-		
-	}
-	
-	public static RodadaAppService getSoleInstance() {
-		if (soleInstance == null) {
-      soleInstance = new RodadaAppService(null, null, null);
+    private static RodadaAppService soleInstance;
+
+    private RodadaRepository rodadaRepository;
+    private RodadaFactory rodadaFactory;
+    private JogadorRepository jogadorRepository;
+
+    private RodadaAppService(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
+        this.rodadaRepository = rodadaRepository;
+        this.rodadaFactory = rodadaFactory;
+        this.jogadorRepository = jogadorRepository;
     }
-		return soleInstance;
-	}
-	
-	public Rodada novaRodadaPorId(String idJogador) throws JogadorNaoEncontradoException {
-	
-		Long id = Long.parseLong(idJogador);
-		
-		try {
-			Jogador retorno = jogadorRepository.getPorId(id);
-			return rodadaFactory.getRodada(retorno);
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
-		}
-		
-		throw new JogadorNaoEncontradoException("Não foi possível encontrar o jogador" );
-	}
-	
-	public Rodada novaRodadaPorNome(String nomeJogador) throws JogadorNaoEncontradoException{
-		
-		try {
-			Jogador retorno = jogadorRepository.getPorNome(nomeJogador);
-			return rodadaFactory.getRodada(retorno);
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
-		}
-		
-		throw new JogadorNaoEncontradoException("Não foi possível encontrar o jogador" );
-	}
-	
-	public Rodada novaRodada(Jogador jogador) {
-		try {
-			jogadorRepository.inserir(jogador);	
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
-		}
-		
-		return null;
-	}
-	
-	public void salvarRodada(Rodada rodada) throws RepositoryException{
-		try {
-			rodadaRepository.inserir(rodada);
-		} catch (Exception e) {
-			System.out.print(e.getMessage());
-		}
-	}
+
+    public void createSoleInstance(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository, JogadorRepository jogadorRepository) {
+
+    }
+
+    public static RodadaAppService getSoleInstance() {
+        if (soleInstance == null) {
+            soleInstance = new RodadaAppService(null, null, null);
+        }
+        return soleInstance;
+    }
+
+    public Rodada novaRodadaPorId(String idJogador) throws JogadorNaoEncontradoException {
+
+        Long id = Long.parseLong(idJogador);
+
+        try {
+            Jogador retorno = jogadorRepository.getPorId(id);
+            return rodadaFactory.getRodada(retorno);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+
+        throw new JogadorNaoEncontradoException("Não foi possível encontrar o jogador" );
+    }
+
+    public Rodada novaRodadaPorNome(String nomeJogador) throws JogadorNaoEncontradoException{
+
+        try {
+            Jogador retorno = jogadorRepository.getPorNome(nomeJogador);
+            return rodadaFactory.getRodada(retorno);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+
+        throw new JogadorNaoEncontradoException("Não foi possível encontrar o jogador" );
+    }
+
+    public Rodada novaRodada(Jogador jogador) {
+        try {
+            jogadorRepository.inserir(jogador);	
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+
+        return null;
+    }
+
+    public void salvarRodada(Rodada rodada) throws RepositoryException{
+        try {
+            rodadaRepository.inserir(rodada);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
 }
